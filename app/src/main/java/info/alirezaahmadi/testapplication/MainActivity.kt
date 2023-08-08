046daf991357eaea5930b84be20385490a70b465
AlirezaAhmadi <ahmadi056.79@gmail.com>
2023-08-08 15:39:41 +0330

    add binding library

--- app/src/main/java/info/alirezaahmadi/testapplication/MainActivity.kt
+++ app/src/main/java/info/alirezaahmadi/testapplication/MainActivity.kt
@@ -2,10 +2,17 @@ package info.alirezaahmadi.testapplication
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import info.alirezaahmadi.testapplication.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
     }
+
 }
